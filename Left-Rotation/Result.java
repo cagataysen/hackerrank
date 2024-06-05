e996029d48c7b61b70c3b5833d06eda1a93c16ae
Cagatay Sen <orhancagataysen@outlook.com>
2024-06-05 11:55:15 +0200

    left rotation added

---
+++ Left-Rotation/Result.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Write your code here
+        List<Integer> result = new ArrayList<Integer>();
+
+        d = d % arr.size();
+
+
+        for(int i = d; i < arr.size(); i++ ){
+            result.add(arr.get(i));
+        }
+
+        for(int i = 0; i < d; i++){
+            result.add(arr.get(i));
+        }
+
+
+        return result;
+    }
+
+}
+
---
+++ Left-Rotation/SolutionLeftRotation.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionLeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
